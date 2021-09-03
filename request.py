17a0dec982d3a5b4a394a712c0213c0dacedbf2e
TempO Riz <tempo2riz@gmail.com>
2021-09-03 16:03:17 +0200

    json

--- request.py
+++ request.py
@@ -1,10 +1,12 @@
 import requests
+import sys
 
+sys.stdout=open('declare.js','w')
 URL = "http://192.168.4.1"
 
 while(True):
     r = requests.get(url = URL)
-    print(r.json())
+    print("var xyz = "+r.text)
 
 
 #print(r.status_code)
