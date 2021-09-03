f48dda3378a29ad33995fa5dde3c25ce25eefdd1
TempO Riz <tempo2riz@gmail.com>
2021-09-03 16:08:43 +0200

    json

--- request.py
+++ request.py
@@ -1,12 +1,14 @@
 import requests
 import sys
 
-sys.stdout=open('declare.js','w')
+# sys.stdout=open('declare.js','w')
 URL = "http://192.168.4.1"
 
 while(True):
     r = requests.get(url = URL)
-    print("var xyz = "+r.text)
+    file = open('declare.js','w')
+    file.write("var xyz = "+r.text)
+    file.close()
 
 
 #print(r.status_code)
